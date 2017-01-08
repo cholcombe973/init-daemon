ab420e9b746dc756d169ab24a54abb888e637668
Chris Holcombe <xfactor973@gmail.com>
2017-01-08 11:03:11 -0800

    Implment detection

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,33 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
+use std::path::Path;
+use std::process::Command;
+
+/// Various init daemons
+pub enum Daemon {
+    /// Most systems are using systemd these days.  Those include:
+    /// Arch, Debian, Fedora, Gentoo, Mageia, OpenSUSE, Ubuntu and probably others
+    Systemd,
+    /// Older versions of Ubuntu
+    Upstart,
+    /// Any daemon that we don't know how to detect yet
+    Unknown,
+}
+
+pub fn detect_daemon() -> Result<Daemon, String> {
+    // If this folder exists it is likely a systemd system
+    if Path::new("/run/systemd/system").exists() {
+        return Ok(Daemon::Systemd);
     }
+    if Path::new("/sbin/initctl").exists() {
+        // Check for upstart
+        let mut cmd = Command::new("/sbin/initctl");
+        cmd.arg("--version");
+        let output = cmd.output().map_err(|e| e.to_string())?;
+        if output.status.success() {
+            let output_str = String::from_utf8_lossy(&output.stdout).into_owned();
+            if output_str.contains("upstart") {
+                return Ok(Daemon::Upstart);
+            }
+        }
+    }
+    return Ok(Daemon::Unknown);
 }
